d20629164208c66a6e18b9a37c1bc86a6c3c6726
duck-ach <djagmlfk@naver.com>
2025-03-26 18:51:02 +0900

    feat: Create Project and Basic Setting

---
+++ src/main/java/com/duckach/yumly/YumlyApplication.java
@@ -0,0 +1,13 @@
+package com.duckach.yumly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YumlyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YumlyApplication.class, args);
+    }
+
+}
