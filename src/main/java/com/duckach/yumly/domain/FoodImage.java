761a8af32b350fb567a2f35e18fc45c5260a5d54
duck-ach <djagmlfk@naver.com>
2025-03-31 22:57:25 +0900

    feat: JPA Create Table FOOD, FOOD_IMG, TAG

--- src/main/java/com/duckach/yumly/domain/FoodImage.java
+++ src/main/java/com/duckach/yumly/domain/FoodImage.java
@@ -1,9 +1,26 @@
 package com.duckach.yumly.domain;
 
+import jakarta.persistence.*;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "FOOD_IMAGE")
+@NoArgsConstructor
 public class FoodImage {
+    @Id @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "FOOD_IMG_ID")
     private int foodImgId;
+
+    @Column(name = "FOOD_IMG_PATH")
     private String foodImgPath;
+
+    @Column(name = "FOOD_IMG_ORIGIN")
     private String foodImgOrigin;
+
+    @Column(name = "FOOD_IMG_FILESYSTEM")
     private String foodImgFileSystem;
-    private Food foodId;
+
+    @ManyToOne // 여러개의 이미지가 하나의 음식
+    @JoinColumn(name = "FOOD_ID")
+    private Food food;
 }
--- src/main/java/com/duckach/yumly/domain/Tag.java
+++ src/main/java/com/duckach/yumly/domain/Tag.java
@@ -1,6 +1,16 @@
 package com.duckach.yumly.domain;
 
+import jakarta.persistence.*;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "TAG")
+@NoArgsConstructor
 public class Tag {
+    @Id @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "TAG_ID")
     private int tagId;
+
+    @Column(name = "TAG_NAME")
     private String tagName;
 }
