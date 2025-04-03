2e974661facea7993f2648b4533a6b96dc19e938
duck-ach <djagmlfk@naver.com>
2025-04-03 22:43:14 +0900

    feat: controller 이슈 해결

--- src/main/java/com/duckach/yumly/controller/FoodController.java
+++ src/main/java/com/duckach/yumly/controller/FoodController.java
@@ -2,6 +2,8 @@ package com.duckach.yumly.controller;
 
 import com.duckach.yumly.domain.Food;
 import com.duckach.yumly.service.FoodService;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -27,8 +29,14 @@ public class FoodController {
         return "food/foodList";
     }
 
-    @GetMapping("add")
+    @GetMapping("/add")
     public String addFood(Model model) {
+        return "food/addFood";
+    }
+
+    @PostMapping("saveFood")
+    public String saveFood(HttpServletRequest request, HttpServletResponse response) {
+
         return "food/addFood";
     }
 }
