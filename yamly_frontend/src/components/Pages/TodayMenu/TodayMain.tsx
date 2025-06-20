c35dd42c03d512eaab73ed428a6660281e9cb3e6
duck-ach <djagmlfk@naver.com>
2025-06-20 17:29:27 +0900

    feat: favicon 추가 및 title 변경

--- yamly_frontend/src/components/Pages/TodayMenu/TodayMain.tsx
+++ yamly_frontend/src/components/Pages/TodayMenu/TodayMain.tsx
@@ -1,6 +1,11 @@
 import HotMenu from "./section/HotMenu";
 
+import { useEffect } from "react";
+
 const TodayMain = () => {
+  useEffect(() => {
+    document.title = "🍱 오늘의 메뉴 - Yamly";
+  }, []);
   return (
     <>
       <div>
