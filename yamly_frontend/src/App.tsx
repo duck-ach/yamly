0b0802947c925cceb0f6ae4a4ad3aae17d9284af
duck-ach <djagmlfk@naver.com>
2025-06-20 16:52:14 +0900

    fix: vercel 배포 시 대소문자 이슈

--- yamly_frontend/src/App.tsx
+++ yamly_frontend/src/App.tsx
@@ -4,7 +4,7 @@ import GamesMain from "./components/Pages/Games/GamesMain";
 import SettingMain from "./components/Pages/Setting/SettingMain";
 import MyPageMain from "./components/Pages/MyPage/MyPageMain";
 import TopMenu from "./components/Layout/TopMenu";
-import LeftMenu from "./components/Layout/Leftmenu";
+import LeftMenu from "./components/Layout/LeftMenu";
 
 function App() {
   return (
