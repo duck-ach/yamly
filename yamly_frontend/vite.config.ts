d4861adcb15daf4b23ab749ad5087dd0afa48f71
duck-ach <djagmlfk@naver.com>
2025-06-20 17:00:09 +0900

    fix: backend 폴더 -> dist 폴더에 빌드되게 변경

--- yamly_frontend/vite.config.ts
+++ yamly_frontend/vite.config.ts
@@ -1,15 +1,17 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
+    outDir: "dist",
+    /* 이것은 backend랑 연동때만
     outDir: path.resolve(
       __dirname,
       "../yamly_backend/src/main/resources/static"
     ),
     emptyOutDir: true,
+    */
   },
 });
